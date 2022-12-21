a57c9267b2f970f4c581693138c2f2a878b6e239
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2022-12-21 01:05:42 +0100

    Update slide4, Implement slide6
    slide4: images overlay on bg img

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+console.log('scrollY', value);
+
 let hubbleRays = document.querySelector('.pg5-hubble-rays');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    console.log('scrollY', value);
-
     if (value>2858) {
         webbRays.style.animation = 'slide-back 1s ease-out forwards';
     } 
@@ -13,8 +13,6 @@ let webbRays = document.querySelector('.pg5-webb-rays');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    console.log('scrollY', value);
-
     if (value>2822) {
         webbRays.style.animation = 'slide-back 1s ease-out forwards';
     } 
