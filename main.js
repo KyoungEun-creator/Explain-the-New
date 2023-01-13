8cdefcf67035f15c7b07782861ca14c01ede9ace
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-01-13 02:14:00 +0100

    Implement slide2
    wipe transition

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+// page2
+var ctx = c.getContext("2d"),
+img = new Image();         // image to show
+
+img.onload = start;
+img.src = "weic2216f.jpg";
+
+function start() {
+  c.onmousemove = function(e) {
+    var r = c.getBoundingClientRect(),
+        x = e.clientX - r.left;
+    ctx.clearRect(0, 0, c.width, c.height); 
+    ctx.drawImage(img, 0, 0, x, c.height,  0, 0, x, c.height); 
+    ctx.fillRect(x, 0, 5, c.height); 
+  };
+}
+
+
+// page
 console.log('scrollY', value);
 
 let hubbleRays = document.querySelector('.pg5-hubble-rays');
