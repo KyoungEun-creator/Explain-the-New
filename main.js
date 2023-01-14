8bdec1c9a69ab2c8642234d589559eacffd3ac0d
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-01-14 23:49:04 +0100

    Update slide6
    insert all the factors in slide6

--- main.js
+++ main.js
@@ -17,22 +17,28 @@ function start() {
 
 
 // page
-console.log('scrollY', value);
+// console.log('scrollY', value);
 
-let hubbleRays = document.querySelector('.pg5-hubble-rays');
+// let hubbleRays = document.querySelector('.pg5-hubble-rays');
 
-window.addEventListener('scroll', function(){
-    let value = window.scrollY;
-    if (value>2858) {
-        webbRays.style.animation = 'slide-back 1s ease-out forwards';
-    } 
-})
+// window.addEventListener('scroll', function(){
+//     let value = window.scrollY;
+//     if (value>2858) {
+//         webbRays.style.animation = 'slide-back 1s ease-out forwards';
+//     } 
+// })
 
-let webbRays = document.querySelector('.pg5-webb-rays');
+// let webbRays = document.querySelector('.pg5-webb-rays');
 
-window.addEventListener('scroll', function(){
-    let value = window.scrollY;
-    if (value>2822) {
-        webbRays.style.animation = 'slide-back 1s ease-out forwards';
-    } 
+// window.addEventListener('scroll', function(){
+//     let value = window.scrollY;
+//     if (value>2822) {
+//         webbRays.style.animation = 'slide-back 1s ease-out forwards';
+//     } 
+// })
+
+// page6
+
+document.querySelector('.secondary_mirror_w').addEventListener('click', function(){
+    document.querySelector('.secondary_mirror_y').style.display='block';
 })
